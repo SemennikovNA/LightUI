4dff48d0099143012f021c457a8be768b5b67bc0
Nikita Semennikov <semennikovna@ya.ru>
2021-12-19 14:30:50 +0300

    Initial Commit

---
+++ LightUI/LightUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightUIApp.swift
+//  LightUI
+//
+//  Created by Никита on 19.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct LightUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
